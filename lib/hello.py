382e6b36341fab1beeec34d16184c130a1356a0c
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-08-01 11:33:32 +0100

    Add new tests for non string params

--- lib/hello.py
+++ lib/hello.py
@@ -1,2 +1,5 @@
 def hello(name):
+    if not isinstance(name, str):
+        raise Exception('You can only pass strings into hello() function')
+    
     return f"Hello {name}, how are you today?"
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,8 +1,19 @@
 from lib.hello import *
+import pytest
 
 def test_hello_takes_a_name_and_says_hello_name():
     actual = hello('Jordan')
 
     expected = 'Hello Jordan, how are you today?'
+
+    assert actual == expected
+
+def test_hello_raises_error_when_not_string_passed_in():
+    with pytest.raises(Exception) as err:
+        say_hello = hello(13)
+    
+    actual = str(err.value)
+    
+    expected = 'You can only pass strings into hello() function'
 
     assert actual == expected
